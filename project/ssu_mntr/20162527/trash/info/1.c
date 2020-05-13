f0572a7394d8d189e8ab591331d44059f00eb287
Ingyu Ham <hfire1207@gmail.com>
2020-05-13 20:27:09 +0900

    캡쳐 추가, 테스트파일 추가

---
+++ project/ssu_mntr/20162527/trash/info/1.c
@@ -0,0 +1,4 @@
+[Trash info]
+/home/ingyu/LSP/project/ssu_mntr/20162527/check/1.c
+D : 2020-05-13 20:21:45
+M : 2020-05-13 20:18:23
---
+++ project/ssu_mntr/20162527/trash/info/3.c
@@ -0,0 +1,4 @@
+[Trash info]
+/home/ingyu/LSP/project/ssu_mntr/20162527/check/3.c
+D : 2020-05-13 20:21:53
+M : 2020-05-13 20:19:11
